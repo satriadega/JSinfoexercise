ab5b15e2cd3061f2c997248f620f658dbba38b65
huntr <satria.pandega@gmail.com>
2023-05-28 13:07:06 +0700

    map and set

---
+++ mapandset/1.js
@@ -0,0 +1,18 @@
+function unique(arr) {
+    /* your code */
+    return Array.from(new Set(arr));
+}
+
+let values = [
+    "Hare",
+    "Krishna",
+    "Hare",
+    "Krishna",
+    "Krishna",
+    "Krishna",
+    "Hare",
+    "Hare",
+    ":-O",
+];
+
+console.log(unique(values)); // Hare, Krishna, :-O
---
+++ mapandset/arrayfrom.js
@@ -0,0 +1,2 @@
+console.log(Array.from("abc"));
+console.log(Array.from([1, 2, "test"]));
